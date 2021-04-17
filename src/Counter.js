8311512456abdeb9cf5ad5b138e7b9a58e2987f5
Katy-fry <katyvfry@gmail.com>
2021-04-17 13:35:15 -0500

    counter app

---
+++ src/Counter.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+
+export default function Counter({ appName, counterValue }) {
+    const [counter, setCounter] = useState(Number(counterValue));
+    const [userInput, setUserInput] = useState(0);
+    const [counterName, setCounterName] = useState(appName);
+    
+    return (
+      <div>
+      <input onChange={(e) => setCounterName(e.target.value)}></input>
+      <h1>{counterName}</h1>
+      <h2>{counter}</h2>
+      
+      <button 
+        onClick={() => setCounter((prevState) => prevState + userInput)}>
+          Increment by {userInput}
+      </button>
+  
+      <input 
+        onChange={(e) => setUserInput(Number(e.target.value))}>
+      </input><br />
+  
+      {counter <= 0 ? '' : 
+        <button onClick={() => setCounter((prevState) => prevState - userInput)}>Decrement by {userInput}
+        </button>}
+  
+      <button 
+        onClick={() => setCounter(0)}>
+          Reset
+      </button>
+      </div>
+    );
+  }
