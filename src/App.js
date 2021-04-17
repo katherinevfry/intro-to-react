c85628f5832c05de7e8d1c5b29630cd812a56a1a
Katy-fry <katyvfry@gmail.com>
2021-04-17 13:47:50 -0500

    changes

--- src/App.js
+++ src/App.js
@@ -3,11 +3,26 @@ import './App.css';
 import Counter from './Counter';
 
 const App = () => {
+
+const array = [
+  {
+    appName: "Counter 1",
+    counterValue: 200
+  },
+  {
+    appName: "Counter 2",
+    counterValue: 0
+  },
+  {
+    appName: "Counter 3",
+    counterValue: 100
+  } 
+];
+
   return (
     <>
-    <Counter appName="Counter 1" counterValue={200} />
-    <Counter appName="Counter 2" counterValue={200} />
-    <Counter appName="Counter 3" counterValue={200} />
+    { array.map((counter, i) => <Counter key={i} appName={counter.appName} counterValue={counter.counterValue} />)
+    }
     </>
   )
 }
